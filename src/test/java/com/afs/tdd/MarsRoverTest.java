0b7f8fd2b6fc97653c7d77126b105c1e2012e7b3
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-15 17:09:55 +0800

    [MarsRover][Andrey]test: Create Unit Test Case for validating movement instructions

---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,21 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarsRoverTest {
+    @Test
+    void should_return_true_when_validate_movement_instructions_is_M() {
+        //given
+        List<String> movementInstructions = null;
+        movementInstructions.add("M");
+        //when
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        //then
+        assertEquals(true,marsRover.validateMovementInstructions(movementInstructions));
+    }
+}

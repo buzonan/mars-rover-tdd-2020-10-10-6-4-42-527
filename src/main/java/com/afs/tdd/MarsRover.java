b51be8f8a132b30ccecbe7b2f6d46456ce190ea9
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-15 22:08:52 +0800

    [MarsRover][Andrey]refactor: instantiate hard coded string and integer 0 to constant variables.

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -5,13 +5,21 @@ import java.util.stream.IntStream;
 
 public class MarsRover {
 
+    public static final String NORTH = "N";
+    public static final String SOUTH = "S";
+    public static final String EAST = "E";
+    public static final String WEST = "W";
+    public static final String MOVE = "M";
+    public static final String LEFT = "L";
+    public static final String RIGHT = "R";
+    public static final int FIRST_INDEX = 0;
     private int locationX;
     private int locationY;
     private String currentDirection;
 
 
-    private String[] movementList = new String[]{"M", "L", "R"};
-    private String[] directionList = new String[]{"N", "E", "S", "W"};
+    private String[] movementList = new String[]{MOVE, LEFT, RIGHT};
+    private String[] directionList = new String[]{NORTH, EAST, SOUTH, WEST};
 
     public MarsRover(int locationX, int locationY, String currentDirection) {
         this.locationX = locationX;
@@ -24,23 +32,23 @@ public class MarsRover {
     }
 
     public void executeMovementInstruction(String movementInstruction) {
-        if(movementInstruction == "M"){
+        if(movementInstruction.equals(MOVE)){
             move();
         }
-        if(movementInstruction.equals("L")){
+        if(movementInstruction.equals(LEFT)){
             turnLeft();
         }
-        if(movementInstruction.equals("R")){
+        if(movementInstruction.equals(RIGHT)){
             turnRight();
         }
     }
 
     private void turnRight() {
-        int directionIndex = IntStream.range(0, directionList.length)
+        int directionIndex = IntStream.range(FIRST_INDEX, directionList.length)
                 .filter(index-> directionList[index].equals(currentDirection))
                 .map(direction->{
                     if(direction == directionList.length-1){
-                        return 0;
+                        return FIRST_INDEX;
                     }
                     return ++direction;
                 })
@@ -51,10 +59,10 @@ public class MarsRover {
     }
 
     private void turnLeft() {
-        int directionIndex = IntStream.range(0, directionList.length)
+        int directionIndex = IntStream.range(FIRST_INDEX, directionList.length)
                 .filter(index-> directionList[index].equals(currentDirection))
                 .map(direction->{
-                    if(direction == 0){
+                    if(direction == FIRST_INDEX){
                         return directionList.length-1;
                     }
                     return --direction;
@@ -76,10 +84,10 @@ public class MarsRover {
     }
 
     private void updateLocationY(String currentDirection) {
-        if (currentDirection.equals("N")) {
+        if (currentDirection.equals(NORTH)) {
             locationY++;
         }
-        if (currentDirection.equals("S")){
+        if (currentDirection.equals(SOUTH)){
             locationY--;
         }
     }

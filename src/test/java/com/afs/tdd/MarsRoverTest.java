e84b8e9880b96681e8ac538da119dd513fc83168
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-15 19:33:39 +0800

    [MarsRover][Andrey]test: Create Unit Test Case for update LocationY given heading is N and movement instruction is M.

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -31,4 +31,19 @@ class MarsRoverTest {
         assertEquals(false,marsRover.validateMovementInstructions(movementInstruction));
 
     }
+
+    @Test
+    void should_return_y_1_when_execute_movement_instruction_given_x_0_y_0_direction_N_and_movement_instruction_M() {
+        //given
+        String movementInstruction = "M";
+
+        //when
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        marsRover.executeMovementInstruction(movementInstruction);
+        //then
+        assertEquals(1,marsRover.getLocationY());
+
+    }
+
+
 }

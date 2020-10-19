f377b09a88a18b14ed593b0fb2c6cd88601203da
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-20 02:04:53 +0800

    [MarsRover][Andrey]refactor: remove unused classes

--- src/main/java/com/afs/tdd/MarsRover/MarsRoverActions.java
+++
@@ -1,7 +0,0 @@
-package com.afs.tdd.MarsRover;
-
-public interface MarsRoverActions {
-    public void move();
-    public void turnLeft();
-    public void turnRight();
-}
--- src/main/java/com/afs/tdd/MarsRover/MarsRoverExecute.java
+++
@@ -1,5 +0,0 @@
-package com.afs.tdd.MarsRover;
-
-public interface MarsRoverExecute {
-    public void execute();
-}
